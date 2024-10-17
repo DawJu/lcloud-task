3d167cd848af101758b2523284bca3a4bb9391ec
DawJu <dwdjuszkiewicz@gmail.com>
2024-10-17 15:38:49 +0200

    Add filter function

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import re
 from s3 import s3_client, bucket_name, bucket_prefix
 
 def list_all_files():
@@ -14,3 +15,13 @@ def upload_file(local_file_path, s3_file_name):
         print(f'File {local_file_path} uploaded to {bucket_prefix}/{s3_file_name}')
     except Exception as e:
         print(f'Error uploading file: {e}')
+
+def filter_files(filter):
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=bucket_prefix)
+    if 'Contents' in response:
+        regex = re.compile(filter)
+        for obj in response['Contents']:
+            if regex.search(obj['Key']):
+                print(obj['Key'])
+    else:
+        print('Found no files.')
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import argparse
-from functions import list_all_files, upload_file
+from functions import list_all_files, upload_file, filter_files
 
 def main():
 
     parser = argparse.ArgumentParser(description='S3 bucket CLI')
     parser.add_argument('--list', action='store_true', help='List all files in the bucket')
     parser.add_argument('--upload', nargs=2, metavar=('local_file', 'bucket_location'), help='Upload a file to the bucket')
+    parser.add_argument('--filter', metavar='regex', help='List all files in the bucket that match the filter regex')
 
     args = parser.parse_args()
 
@@ -13,6 +14,8 @@ def main():
         list_all_files()
     elif args.upload:
         upload_file(args.upload[0], args.upload[1])
+    elif args.filter:
+        filter_files(args.filter[0])
 
 if __name__ == "__main__":
     main()
